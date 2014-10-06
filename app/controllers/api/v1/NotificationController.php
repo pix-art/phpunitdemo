3fd9f9b0095427098963a23c5ab7286dc440a9c5
Joris Hens <joris@goodbytes.be>
2014-10-06 19:58:14 +0200

    Api finished + added a small mobile app
    Signed-off-by: Joris Hens <joris@goodbytes.be>

--- app/controllers/api/v1/NotificationController.php
+++ app/controllers/api/v1/NotificationController.php
@@ -5,7 +5,15 @@ class NotificationController extends \BaseController {
 	public function index()
 	{
 		//return "hello API v1!";
-		$response = array("status" => "success", "notifications" => \Notification::get());
+		$response = array("status" => "success", "notifications" => \Notification::with('user')->get());
+		return \Response::json($response);
+	}
+
+	public function show($id)
+	{
+		// show only one specific notification
+		$notification = \Notification::find($id);
+		$response = array("status" => "success", "notification" => $notification);
 		return \Response::json($response);
 	}
 }
--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,10 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+
+	public function notifications()
+    {
+        return $this->hasMany('Notification');
+    }
+
 }
