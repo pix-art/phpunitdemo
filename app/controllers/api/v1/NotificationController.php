6323b3564edb44688d1698cd26c904315995ac06
Joris Hens <joris@goodbytes.be>
2014-10-06 19:14:39 +0200

    return all notifications
    Signed-off-by: Joris Hens <joris@goodbytes.be>

--- app/controllers/api/v1/NotificationController.php
+++ app/controllers/api/v1/NotificationController.php
@@ -4,6 +4,8 @@ class NotificationController extends \BaseController {
 
 	public function index()
 	{
-		return "hello API v1!";
+		//return "hello API v1!";
+		$response = array("status" => "success", "notifications" => \Notification::get());
+		return \Response::json($response);
 	}
 }
